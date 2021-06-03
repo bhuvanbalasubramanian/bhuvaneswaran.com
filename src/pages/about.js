be0d979f3ecdc49afa6141d6f03d4eb416f2eaf9
Bhuvaneswaran Balasubramanian <bhuvanesad@gmail.com>
2021-06-04 02:50:42 +0530

    images for devs

--- src/pages/about.js
+++ src/pages/about.js
@@ -20,10 +20,10 @@ const AboutPage = () => (
     </p>
 
     <p>
-      Being a full stack developer, I have worked in various tools and programming
-      languages. Java and Javascript are my favourite languages.
+      Being a full stack developer, I have worked in various tools and
+      programming languages. Java and Javascript are my favourite languages.
     </p>
-
+    
     <p>
       You can contact me by email at{" "}
       <a href="mailto:hello@bhuvaneswaran.com">hello@bhuvaneswaran.com</a> to
@@ -33,71 +33,148 @@ const AboutPage = () => (
     <h3>
       Skills{" "}
       <span role="img" aria-label="swimming">
-      💻
+        💻
       </span>
     </h3>
     <div className="about">
-    <ul>
-      <li>Languages: Node.js, React, Java, JavaScript, TypeScript, HTML5, CSS3</li>
-      <li>Frameworks: Express.js, Nest.js, Spring Boot</li>
-      <li>Database: MySql, MongoDB</li>
-      <li>Web Services: REST API, Microservice Architecture</li> 
-      <li>Messaging System: Apache Kafka, Redis Cache, Avro</li>
-      <li>Test Automation: Protractor, Cypress</li>
-      <li>DevOps: CI/CD</li>     
-      <li>Cloud: Microsoft Azure</li>      
-    </ul>
+      <ul>
+        <li>
+          Languages: Node.js, React, Java, JavaScript, TypeScript, HTML5, CSS3
+        </li>
+        <li>Frameworks: Express.js, Nest.js, Spring Boot</li>
+        <li>Database: MySql, MongoDB</li>
+        <li>Web Services: REST API, Microservice Architecture</li>
+        <li>Messaging System: Apache Kafka, Redis Cache, Avro</li>
+        <li>Test Automation: Protractor, Cypress</li>
+        <li>Cloud: Microsoft Azure</li>
+      </ul>
+
+      <div>
+      <img
+        src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original.svg"
+        alt="nodejs"
+        width="50px"
+        height="50px"
+      />
+      <img
+        src="https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg"
+        alt="java"
+        width="50px"
+        height="50px"
+      />
+      <img
+        src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg"
+        alt="react"
+        width="50px"
+        height="50px"
+      />
+      <img
+        src="https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg"
+        alt="typescript"
+        width="50px"
+        height="50px"
+      />
+      <img
+        src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
+        alt="javascript"
+        width="50px"
+        height="50px"
+      />
+      <img
+        src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg"
+        alt="html5"
+        width="50px"
+        height="50px"
+      />
+      <img
+        src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg"
+        alt="css3"
+        width="50px"
+        height="50px"
+      />
+      <img
+        src="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original.svg"
+        alt="css3"
+        width="50px"
+        height="50px"
+      />
+      <img
+        src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nestjs/nestjs-plain.svg"
+        alt="nestjs"
+        width="50px"
+        height="50px"
+      />
+      <img
+        src="https://raw.githubusercontent.com/devicons/devicon/master/icons/spring/spring-original.svg"
+        alt="spring"
+        width="50px"
+        height="50px"
+      />
+      <img
+        src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original.svg"
+        alt="mongodb"
+        width="50px"
+        height="50px"
+      />
+      <img
+        src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original.svg"
+        alt="mysql"
+        width="50px"
+        height="50px"
+      />
+    </div>
+
     </div>
 
     <h3>
       Certifications{" "}
       <span role="img" aria-label="laptop">
-      📜 {" "}
+        📜{" "}
       </span>
     </h3>
 
     <div className="about">
       <ul>
-        <li>
-          <a
-            href="https://www.credly.com/badges/3b67a953-13e4-479e-bd50-8863ec21aa85"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Microsoft Certified: DevOps Engineer Expert
-          </a>
-         
-        </li>
-        <li>
-          <a
-            href="https://www.credly.com/badges/4644f28a-4e5c-4710-8ec2-4d72ce333081"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Microsoft Certified: Azure Developer Associate
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.credly.com/badges/dafb9f1c-b03e-443c-b609-768376f355d1"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Certified TOGAF Enterprise Architect
-          </a>
-        </li>
+        <li>Microsoft Certified: DevOps Engineer Expert</li>
+        <li>Microsoft Certified: Azure Developer Associate</li>
+        <li>Certified TOGAF Enterprise Architect</li>
         <li>Certified Scrum Developer® (CSD)</li>
       </ul>
     </div>
     <div>
-      <div className="badge"><div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="3b67a953-13e4-479e-bd50-8863ec21aa85" data-share-badge-host="https://www.credly.com"></div></div>
-      <div className="badge"><div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="4644f28a-4e5c-4710-8ec2-4d72ce333081" data-share-badge-host="https://www.credly.com"></div></div>
-      <div className="badge"><div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="07ca4339-fd7a-46c0-84f8-3fa7216d8143" data-share-badge-host="https://www.credly.com"></div></div>
+      <div className="badge">
+        <div
+          data-iframe-width="150"
+          data-iframe-height="270"
+          data-share-badge-id="3b67a953-13e4-479e-bd50-8863ec21aa85"
+          data-share-badge-host="https://www.credly.com"
+        ></div>
+      </div>
+      <div className="badge">
+        <div
+          data-iframe-width="150"
+          data-iframe-height="270"
+          data-share-badge-id="4644f28a-4e5c-4710-8ec2-4d72ce333081"
+          data-share-badge-host="https://www.credly.com"
+        ></div>
+      </div>
+      <div className="badge">
+        <div
+          data-iframe-width="150"
+          data-iframe-height="270"
+          data-share-badge-id="07ca4339-fd7a-46c0-84f8-3fa7216d8143"
+          data-share-badge-host="https://www.credly.com"
+        ></div>
+      </div>
       <Helmet>
-      <script type="text/javascript" async src="//cdn.credly.com/assets/utilities/embed.js"></script>
+        <script
+          type="text/javascript"
+          async
+          src="//cdn.credly.com/assets/utilities/embed.js"
+        ></script>
       </Helmet>
     </div>
-    <div className='clear'></div>
+    <div className="clear"></div>
     <h3>
       Hobbies{" "}
       <span role="img" aria-label="swimming">
@@ -105,11 +182,11 @@ const AboutPage = () => (
       </span>
     </h3>
     <div className="about">
-    <ul>
-      <li>Cycling: MachCity bike</li>
-      <li>Swimming: Freestyle</li>
-      <li>Gaming console: PS4 pro</li>
-    </ul>
+      <ul>
+        <li>Cycling: MachCity bike</li>
+        <li>Swimming: Freestyle</li>
+        <li>Gaming console: PS4 pro</li>
+      </ul>
     </div>
 
     <h3>
@@ -120,12 +197,12 @@ const AboutPage = () => (
     </h3>
     <p>Few places I travelled,</p>
     <div className="about">
-    <ul>
-      <li>France: Paris</li>
-      <li>Netherlands: Amsterdam</li>
-      <li>Germany: Hamburg</li>
-      <li>Denmark: Copenhagen, Nordborg</li>
-    </ul>
+      <ul>
+        <li>France: Paris</li>
+        <li>Netherlands: Amsterdam</li>
+        <li>Germany: Hamburg</li>
+        <li>Denmark: Copenhagen, Nordborg</li>
+      </ul>
     </div>
     <h3>
       Social{" "}
@@ -141,7 +218,8 @@ const AboutPage = () => (
         rel="noreferrer"
       >
         Twitter
-      </a>{", "}
+      </a>
+      {", "}
       <a
         href="https://www.linkedin.com/in/bhuvan-balasubramanian/"
         target="_blank"
